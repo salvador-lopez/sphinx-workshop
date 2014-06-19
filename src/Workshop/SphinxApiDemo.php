bbe8ea27ac008ca11f53f3db0881a45f39b0a3e4
Salvador Lopez <salvador.lopez@atrapalo.com>
2014-06-19 23:49:08 +0200

    updates sphinx.conf and demos

--- src/Workshop/SphinxApiDemo.php
+++ src/Workshop/SphinxApiDemo.php
@@ -8,9 +8,18 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $client = new SphinxClient();
 
-$client->setServer('localhost', 9312);
+$client->setFieldWeights(
+    [
+        'first_name'       => 100,
+        'last_name'        => 20,
+        'film_description' => 10,
+    ]
+);
 
-// Fulltext Search
-$client->setLimits(0,1);
-$results = $client->query('penelope', 'events');
+$client->setServer('192.168.56.99', 9312);
+$client->setGroupBy('film_id', SPH_GROUPBY_ATTR, '@weight ASC');
+$client->setLimits(0,5);
+$client->setMatchMode(SPH_MATCH_EXTENDED);
+$results = $client->query('@film_description epic | action', 'events');
+
 print_r($results);
--- src/Workshop/SphinxQLDemo.php
+++ src/Workshop/SphinxQLDemo.php
@@ -9,9 +9,17 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 
 $conn = new Connection();
 $conn->setConnectionParams('192.168.56.99', 9306);
-//$conn->setConnectionParams('127.0.0.1', 9306);
 
-$sql = "SELECT * FROM events where MATCH('penelope')";
+$sql = <<<QUERY
+    SELECT * FROM
+        events
+   WHERE MATCH('epic | action')
+   GROUP BY film_id
+   ORDER BY WEIGHT() ASC
+   LIMIT 0,5
+   OPTION field_weights=(first_name=100,last_name=20,film_description=10);
+QUERY;
+
 $query = SphinxQL::create($conn)->query($sql);
 
 $result = $query->execute();
